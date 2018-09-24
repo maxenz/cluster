5d17c120d0574963c07441005254271ba25b46a2
maxenz <maximilianopoggio@gmail.com>
2018-09-23 21:06:24 -0300

    adding routes

--- routes/printers.js
+++ routes/printers.js
@@ -5,10 +5,10 @@ const controller = require('../controllers/printer');
 router.route('/printers')
   .get(controller.index)
   .post(controller.new);
-// router.route('/printers/:printer_id')
-//   .get(controller.view)
-//   .patch(controller.update)
-//   .put(controller.update)
-//   .delete(controller.delete);
+router.route('/printers/:printer_id')
+  .get(controller.view)
+  .patch(controller.update)
+  .put(controller.update)
+  .delete(controller.delete);
 
 module.exports = router;
