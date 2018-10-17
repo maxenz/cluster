d1bd495b413eadcc9c943f47059f1179a402db05
maxenz <maximilianopoggio@gmail.com>
2018-10-17 20:38:29 -0300

    added static build index

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ const port = process.env.PORT || 5000;
 
 app.use('/api/users', users);
 app.use('/api', apiRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.listen(port, function () {
   console.log('Running cluster3d server on port ' + port);
 });
