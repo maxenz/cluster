22b39eac457f22374c0a5d128c473621ef7eb56e
maxenz <maximilianopoggio@gmail.com>
2019-06-26 00:41:25 -0300

    File uploader fixed

--- client/src/components/Requests/CreateEditRequestForm.js
+++ client/src/components/Requests/CreateEditRequestForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   Form,
   Segment,
@@ -7,51 +7,51 @@ import {
   Icon,
   Label,
   Dropdown
-} from 'semantic-ui-react';
-import FileReaderInput from '../../../node_modules/react-file-reader-input';
-import {REQUESTS_STATUS_SENT_BY_USER} from "../../constants/requests";
-import {colorsList, materialsList} from "./requestsList";
+} from "semantic-ui-react";
+import * as filestack from "filestack-js";
+import FileReaderInput from "../../../node_modules/react-file-reader-input";
+import { REQUESTS_STATUS_SENT_BY_USER } from "../../constants/requests";
+import { colorsList, materialsList } from "./requestsList";
 // import {uploadRequestFile} from "../../firebase/storage";
 import moment from "moment/moment";
 import BackButton from "../Common/BackButton";
 
 const styles = {
-  backgroundColor: '#FFF',
+  backgroundColor: "#FFF",
   header: {
-    fontFamily: 'Josefin Sans, cursive',
-    fontSize: '2.5rem',
+    fontFamily: "Josefin Sans, cursive",
+    fontSize: "2.5rem"
   },
   file: {
     label: {
-      backgroundColor: '#f2711c',
-      marginTop: '5px',
-      color: '#FFF',
+      backgroundColor: "#f2711c",
+      marginTop: "5px",
+      color: "#FFF"
     }
   },
   fileButton: {
-    marginBottom: '10px',
-    display: '-webkit-inline-box',
+    marginBottom: "10px",
+    display: "-webkit-inline-box"
   }
 };
 
 const successMessageStyle = {
-  width: '50%',
-  marginLeft: 'auto',
-  marginRight: 'auto',
+  width: "50%",
+  marginLeft: "auto",
+  marginRight: "auto"
 };
 
 class CreateEditRequestForm extends Component {
-
   constructor(props) {
     super(props);
     this._initState = {
-      diameter: '',
-      materialType: '',
-      colorType: '',
+      diameter: "",
+      materialType: "",
+      colorType: "",
       uploadedFile: null,
       error: null,
       isSaving: false,
-      requestWasSent: false,
+      requestWasSent: false
     };
     this.state = this._initState;
   }
@@ -66,16 +66,20 @@ class CreateEditRequestForm extends Component {
     this.setState(newState);
   };
 
-  clearError = () => this.setState({error: null});
+  clearError = () => this.setState({ error: null });
 
   handleFileChange = (e, results) => {
     const [pgEvent, file] = results[0];
-    if (file.name.toUpperCase().endsWith('STL')) {
-      this.setState({uploadedFile: file, fileName: `${moment()}_${file.name}`});
+    if (file.name.toUpperCase().endsWith("STL")) {
+      this.setState({
+        uploadedFile: file,
+        fileName: `${moment()}_${file.name}`
+      });
       this.clearError();
-    }
-    else {
-      this.setState({error: {message: 'Debe subir un archivo con formato .stl'}})
+    } else {
+      this.setState({
+        error: { message: "Debe subir un archivo con formato .stl" }
+      });
     }
   };
 
@@ -83,52 +87,56 @@ class CreateEditRequestForm extends Component {
     if (this.formIsInvalid()) {
       return;
     }
-    this.setState({...this.state, isSaving: true,});
-    const {colorType, materialType, diameter, uploadedFile, fileName} = this.state;
-    this.props.saveRequest(
-        {
-          color_type: colorType,
-          material_type: materialType,
-          diameter,
-          created_by: 1,
-          file_name: fileName,
-          status: REQUESTS_STATUS_SENT_BY_USER,
-          create_date: Date.now(),
-          price: 0,
-        }).then(() => {
-      this.props.cleanState();
-    })
-    // .then(() => {
-    //   uploadRequestFile(uploadedFile, fileName).then(() => {
-    //     this.setState({
-    //       ...this.state,
-    //       isSaving: false,
-    //       requestWasSent: true
-    //     });
-    //   });
-    // });
-  };
-
-  formIsInvalid = () => {
+    this.setState({ ...this.state, isSaving: true });
     const {
-      uploadedFile,
       colorType,
       materialType,
       diameter,
+      uploadedFile,
+      fileName
     } = this.state;
+    const apikey = "ArCGBdA2yRP6sNrIf24e5z";
+
+    const client = filestack.init(apikey);
+
+    console.log("pepe");
+    client
+      .upload(uploadedFile)
+      .then(res => {
+        console.log(res);
+        this.props
+          .saveRequest({
+            color_type: colorType,
+            material_type: materialType,
+            diameter,
+            created_by: 1,
+            file_name: res.url,
+            status: REQUESTS_STATUS_SENT_BY_USER,
+            create_date: Date.now(),
+            price: 0
+          })
+          .then(() => {
+            this.props.cleanState();
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  formIsInvalid = () => {
+    const { uploadedFile, colorType, materialType, diameter } = this.state;
 
     const isInvalid =
-        colorType === '' ||
-        diameter === '' ||
-        materialType === '' ||
-        uploadedFile === null;
+      colorType === "" ||
+      diameter === "" ||
+      materialType === "" ||
+      uploadedFile === null;
 
     return isInvalid;
-
   };
 
   render() {
-
     const {
       uploadedFile,
       colorType,
@@ -136,103 +144,128 @@ class CreateEditRequestForm extends Component {
       diameter,
       error,
       isSaving,
-      requestWasSent,
+      requestWasSent
     } = this.state;
 
-
     return (
-        <div style={{marginTop: '7%'}}>
-          <BackButton handleGoBack={this.props.handleGoBack}/>
-          {
-            !requestWasSent ?
+      <div style={{ marginTop: "7%" }}>
+        <BackButton handleGoBack={this.props.handleGoBack} />
+        {!requestWasSent ? (
+          <Form
+            error
+            size="large"
+            onSubmit={this.onSubmit}
+            style={{
+              width: "50%",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+          >
+            <div
+              style={{
+                height: "50px",
+                width: "100%",
+                backgroundColor: "#f2711c",
+                textAlign: "center"
+              }}
+            >
+              <h3
+                style={{
+                  color: "white",
+                  lineHeight: "50px",
+                  fontSize: "20px"
+                }}
+              >
+                Generar pedido
+              </h3>
+            </div>
+            <Segment
+              stacked
+              style={{
+                borderTopLeftRadius: 0,
+                borderTopRightRadius: 0,
+                marginTop: 0
+              }}
+            >
+              <Dropdown
+                className="formElement"
+                placeholder="Tipo de material"
+                fluid
+                selection
+                value={materialType}
+                options={materialsList}
+                onChange={(event, data) =>
+                  this.handleChange("materialType", data.value)
+                }
+              />
 
-                <Form error size='large' onSubmit={this.onSubmit}
-                      style={{
-                        width: '50%',
-                        marginLeft: 'auto',
-                        marginRight: 'auto'
-                      }}>
-                  <div style={{
-                    height: '50px',
-                    width: '100%',
-                    backgroundColor: '#f2711c',
-                    textAlign: 'center'
-                  }}>
-                    <h3 style={{
-                      color: 'white',
-                      lineHeight: '50px',
-                      fontSize: '20px'
-                    }}>Generar pedido</h3>
-                  </div>
-                  <Segment stacked style={{
-                    borderTopLeftRadius: 0,
-                    borderTopRightRadius: 0,
-                    marginTop: 0
-                  }}>
+              <Dropdown
+                className="formElement"
+                placeholder="Color"
+                fluid
+                selection
+                value={colorType}
+                options={colorsList}
+                onChange={(event, data) =>
+                  this.handleChange("colorType", data.value)
+                }
+              />
 
-                    <Dropdown
-                        className="formElement"
-                        placeholder="Tipo de material"
-                        fluid
-                        selection
-                        value={materialType}
-                        options={materialsList}
-                        onChange={(event, data) => this.handleChange('materialType', data.value)}
-                    />
+              <Form.Input
+                fluid
+                placeholder="Diámetro (cm.)"
+                type="text"
+                icon="resize vertical"
+                iconPosition="left"
+                value={diameter}
+                onChange={event =>
+                  this.handleChange("diameter", event.target.value)
+                }
+              />
 
-                    <Dropdown
-                        className="formElement"
-                        placeholder="Color"
-                        fluid
-                        selection
-                        value={colorType}
-                        options={colorsList}
-                        onChange={(event, data) => this.handleChange('colorType', data.value)}
-                    />
+              <div className="formElement" style={styles.fileButton}>
+                <FileReaderInput
+                  as="binary"
+                  id="my-file-input"
+                  onChange={this.handleFileChange}
+                >
+                  <Button icon labelPosition="left">
+                    <Icon name="attach" />
+                    Seleccione un archivo
+                  </Button>
+                </FileReaderInput>
+                {uploadedFile ? (
+                  <Label style={styles.file.label} as="a">
+                    <Icon name="attach" />
+                    {uploadedFile.name}
+                  </Label>
+                ) : null}
+              </div>
 
-                    <Form.Input
-                        fluid
-                        placeholder='Diámetro (cm.)'
-                        type='text'
-                        icon='resize vertical'
-                        iconPosition='left'
-                        value={diameter}
-                        onChange={event => this.handleChange('diameter', event.target.value)}/>
+              <Button
+                color="orange"
+                fluid
+                size="large"
+                disabled={this.formIsInvalid() || isSaving}
+              >
+                Generar
+              </Button>
 
-                    <div className="formElement" style={styles.fileButton}>
-                      <FileReaderInput as="binary" id="my-file-input"
-                                       onChange={this.handleFileChange}>
-                        <Button icon labelPosition='left'>
-                          <Icon name='attach'/>
-                          Seleccione un archivo
-                        </Button>
-                      </FileReaderInput>
-                      {
-                        uploadedFile ?
-                            <Label style={styles.file.label} as='a'>
-                              <Icon name='attach'/>
-                              {uploadedFile.name}
-                            </Label> : null
-                      }
-                    </div>
-
-                    <Button color='orange' fluid size='large'
-                            disabled={this.formIsInvalid() || isSaving}>Generar</Button>
-
-                    {error ? <Message error header='Error'
-                                      content={error.message}/> : null}
-
-                  </Segment>
-                </Form>
-                : <Message
-                    icon='checkmark'
-                    success
-                    style={successMessageStyle}
-                    header='La petición de impresión fue enviada correctamente.'
-                    content='Se le enviará un email de aviso cuando la cotización esté lista.'
-                />
-          }
-        </div>
+              {error ? (
+                <Message error header="Error" content={error.message} />
+              ) : null}
+            </Segment>
+          </Form>
+        ) : (
+          <Message
+            icon="checkmark"
+            success
+            style={successMessageStyle}
+            header="La petición de impresión fue enviada correctamente."
+            content="Se le enviará un email de aviso cuando la cotización esté lista."
+          />
+        )}
+      </div>
     );
   }
 }
--- client/src/components/Requests/QuotingForm.js
+++ client/src/components/Requests/QuotingForm.js
@@ -1,71 +1,86 @@
-import React from 'react'
-import {Form, Button, Segment, Message} from 'semantic-ui-react';
+import React from "react";
+import { Form, Button, Segment, Message } from "semantic-ui-react";
 import BackButton from "../Common/BackButton";
 
-const QuotingForm = (props) => {
-
-  const {request, formIsInvalid, handleSaveQuoteRequest, fileUrl} = props;
+const QuotingForm = props => {
+  const { request, formIsInvalid, handleSaveQuoteRequest, fileUrl } = props;
 
   return (
-      <div style={{marginTop: '7%'}}>
-        <BackButton handleGoBack={props.handleGoBack}/>
-        <div style={{
-          width: '50%',
-          marginLeft: 'auto',
-          marginRight: 'auto'
-        }}>
-          <Message color='blue'>
-            Podés descargar el archivo adjunto a este pedido haciendo click <a
-              href={fileUrl} download>acá</a>
-          </Message>
-          <Form error size='large' onSubmit={handleSaveQuoteRequest}>
-            <div style={{
-              height: '50px',
-              width: '100%',
-              backgroundColor: '#f2711c',
-              textAlign: 'center'
-            }}>
-              <h3 style={{
-                color: 'white',
-                lineHeight: '50px',
-                fontSize: '20px'
-              }}>
-                Cotizar impresión
-              </h3>
-            </div>
-            <Segment stacked style={{
+    <div style={{ marginTop: "7%" }}>
+      <BackButton handleGoBack={props.handleGoBack} />
+      <div
+        style={{
+          width: "50%",
+          marginLeft: "auto",
+          marginRight: "auto"
+        }}
+      >
+        <Message color="blue">
+          Podés descargar el archivo adjunto a este pedido haciendo click
+          <a href={request.file_name} target="_blank">
+            &nbsp;acá
+          </a>
+        </Message>
+        <Form error size="large" onSubmit={handleSaveQuoteRequest}>
+          <div
+            style={{
+              height: "50px",
+              width: "100%",
+              backgroundColor: "#f2711c",
+              textAlign: "center"
+            }}
+          >
+            <h3
+              style={{
+                color: "white",
+                lineHeight: "50px",
+                fontSize: "20px"
+              }}
+            >
+              Cotizar impresión
+            </h3>
+          </div>
+          <Segment
+            stacked
+            style={{
               borderTopLeftRadius: 0,
               borderTopRightRadius: 0,
               marginTop: 0
-            }}>
+            }}
+          >
+            <Form.Input
+              fluid
+              placeholder="Precio"
+              name="price"
+              type="number"
+              icon="dollar sign"
+              iconPosition="left"
+              value={request.price || ""}
+              onChange={event =>
+                props.handleFormChange(event.target.name, event.target.value)
+              }
+            />
 
-              <Form.Input
-                  fluid
-                  placeholder='Precio'
-                  name='price'
-                  type='number'
-                  icon='dollar sign'
-                  iconPosition='left'
-                  value={request.price || ''}
-                  onChange={event => props.handleFormChange(event.target.name, event.target.value)}/>
+            <Form.Input
+              fluid
+              placeholder="Tiempo en minutos"
+              name="total_printing_time"
+              type="number"
+              icon="clock"
+              iconPosition="left"
+              value={request.total_printing_time || ""}
+              onChange={event =>
+                props.handleFormChange(event.target.name, event.target.value)
+              }
+            />
 
-              <Form.Input
-                  fluid
-                  placeholder='Tiempo en minutos'
-                  name='total_printing_time'
-                  type='number'
-                  icon='clock'
-                  iconPosition='left'
-                  value={request.total_printing_time || ''}
-                  onChange={event => props.handleFormChange(event.target.name, event.target.value)}/>
-
-              <Button disabled={formIsInvalid} color='orange' fluid
-                      size='large'>Cotizar</Button>
-
-            </Segment>
-          </Form>
-        </div>
+            <Button disabled={formIsInvalid} color="orange" fluid size="large">
+              Cotizar
+            </Button>
+          </Segment>
+        </Form>
       </div>
+    </div>
   );
 };
 
